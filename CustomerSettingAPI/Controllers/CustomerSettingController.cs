88edf48f4762eca8bd6cec0f0d5f3d828a8b091d
minhchi2494 <minhchi2494@gmail.com>
2022-01-06 15:04:22 +0700

    Push API

---
+++ CustomerSettingAPI/Controllers/CustomerSettingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CustomerSettingAPI.Entities;
+using CustomerSettingAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerSettingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerSettingController : ControllerBase
+    {
+        private readonly ICustomerSettingService _services;
+
+        public CustomerSettingController(ICustomerSettingService services)
+        {
+            _services = services;   
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<CustomerSetting>> getAll()
+        {
+            return _services.getAll();
+        }
+    }
+}
---
+++ CustomerSettingAPI/Services/ICustomerSettingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CustomerSettingAPI.Entities;
+
+namespace CustomerSettingAPI.Services
+{
+    public interface ICustomerSettingService
+    {
+        Task<IEnumerable<CustomerSetting>> getAll();
+    }
+}
